273f2e22d1067fd7b785f04c1aac8fe3e2e8b56b
Vidhya Mani <vidhyamaniece@gmail.com>
2023-01-16 23:36:59 +0530

    Controller, Model created and DB migration done

--- login_auth/db/migrate/20230116175818_create_useraccounts.rb
+++ login_auth/db/migrate/20230116175818_create_useraccounts.rb
@@ -1,6 +1,9 @@
 class CreateUseraccounts < ActiveRecord::Migration[7.0]
   def change
     create_table :useraccounts do |t|
+      t.string :email
+      t.string :name
+      t.string :password_digest
 
       t.timestamps
     end
