53fca01fba98e69bf441223e160379c70fa9cb15
Vidhya Mani <vidhyamaniece@gmail.com>
2023-01-17 19:28:11 +0530

    Useraccount controller and model coded and data added from postman(post)

--- login_auth/app/controllers/useraccount_controller.rb
+++ login_auth/app/controllers/useraccount_controller.rb
@@ -1,2 +1,27 @@
+require 'bcrypt'
 class UseraccountController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    def index 
+        # current_login = Useraccount.find_by_id(session[:current_login])
+        # render json: current_login
+        render json: Useraccount.all
+    end 
+    def show 
+        v = Useraccount.find(params[:id])
+        render json: v
+    end
+    def create 
+        v = !params[:name].empty? and !params[:email].empty? and params[:password].empty?
+        if v 
+            d = Useraccount.create(
+                'name': params[:name],
+                'email': params[:email],
+                'password': params[:password]
+            )
+            puts d 
+            render json: "Data Added Successfully"
+        else 
+            render json: "Data not added"
+        end
+    end
 end
--- login_auth/app/models/useraccount.rb
+++ login_auth/app/models/useraccount.rb
@@ -1,2 +1,4 @@
 class Useraccount < ApplicationRecord
+    has_secure_password 
+    validates :name, :email, :password, presence: true
 end
